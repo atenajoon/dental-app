c63428d73bf17fba49220962cfc5e4f4cdefedc2
Atena Razm <atenarazm@gmail.com>
2021-07-28 20:16:24 -0700

    CommentCard component created and added to the Slider component

---
+++ src/components/common/commentCard.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CommentCard = () => {
+  return <div className="content-box comment-card">CommentCard</div>;
+};
+
+export default CommentCard;
--- src/components/common/slider.jsx
+++ src/components/common/slider.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import CommentCard from "./commentCard";
 
 const Slider = () => {
-  return <div className="slider-box">Slider</div>;
+  return (
+    <div className="slider-container">
+      {" "}
+      Slider
+      <div className="slider-box">
+        <CommentCard />
+        <CommentCard />
+        <CommentCard />
+      </div>
+    </div>
+  );
 };
 
 export default Slider;
