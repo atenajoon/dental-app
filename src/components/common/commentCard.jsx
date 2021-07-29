f943d9bb107b60ad271265099344babde581b427
Atena Razm <atenarazm@gmail.com>
2021-07-28 20:21:22 -0700

    RatingStars component added to the CommentCard component

--- src/components/common/commentCard.jsx
+++ src/components/common/commentCard.jsx
@@ -1,7 +1,13 @@
 import React from "react";
+import RatingStars from "./ratingStars";
 
 const CommentCard = () => {
-  return <div className="content-box comment-card">CommentCard</div>;
+  return (
+    <div className="content-box comment-card">
+      <RatingStars />
+      CommentCard
+    </div>
+  );
 };
 
 export default CommentCard;
---
+++ src/components/common/ratingStars.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const RatingStars = () => {
+  return (
+    <div className="rating-star-box">
+      <small>RatingStars</small>
+    </div>
+  );
+};
+
+export default RatingStars;
