0e327b8b18d9c395ed19a6ba606935a3eae3ffe0
Atena Razm <atenarazm@gmail.com>
2021-07-28 20:02:33 -0700

    Slider component added to the Testimonial component with basic style

--- src/components/testimonialSection.jsx
+++ src/components/testimonialSection.jsx
@@ -1,7 +1,13 @@
 import React from "react";
+import Slider from "./common/slider";
 
 const TestimonialSection = () => {
-  return <div className="testimonial-sec-box">Testimonial</div>;
+  return (
+    <div className="testimonial-sec-box">
+      Testimonial
+      <Slider />
+    </div>
+  );
 };
 
 export default TestimonialSection;
