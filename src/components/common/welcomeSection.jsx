f45893ba9b93c750641dbc6bd4c90226c421d85e
Atena Razm <atenarazm@gmail.com>
2021-07-28 19:32:43 -0700

    welcome-sec-box added and styled

--- src/components/common/welcomeSection.jsx
+++ src/components/common/welcomeSection.jsx
@@ -1,7 +1,17 @@
 import React from "react";
 
 const WelcomeSection = () => {
-  return <div>Welcome to Diamond Dental Website!</div>;
+  return (
+    <div className="welcome-sec-box">
+      <h3>Who We Are?</h3>
+      <p className="content-box welcome-sec-content">
+        At Diamond Dental Clinic, you are our first priority. From the moment
+        you walk in the door, we make it our mission to meet all your dental
+        needs. We proudly serve patients in ten different languages with
+        extended office hours in a convenient, central Vancouver location.
+      </p>
+    </div>
+  );
 };
 
 export default WelcomeSection;
