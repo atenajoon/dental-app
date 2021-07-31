4db59a85d7c3eaad071bbba975c161e3a4a9fe19
Atena Razm <atenarazm@gmail.com>
2021-07-31 11:52:46 -0700

    banner component got modified with new classNames

--- src/components/Banner/index.jsx
+++ src/components/Banner/index.jsx
@@ -3,8 +3,8 @@ import React from "react";
 const Banner = () => {
   return (
     <header>
-      <div className="bg-image"></div>
-      <div className="bg-text">
+      <div className="banner-image"></div>
+      <div className="banner-text">
         <h2>Welcome to</h2>
         <h1>Diamond Dental Clinic</h1>
         <p>Bringing a bright smile is our job...</p>
