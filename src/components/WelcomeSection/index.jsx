fe46786ec791d13b14ead575103f5b2f72c6a366
Atena Razm <atenarazm@gmail.com>
2021-08-02 13:01:29 -0700

    welcome section got proper className and style

--- src/components/WelcomeSection/index.jsx
+++ src/components/WelcomeSection/index.jsx
@@ -5,13 +5,13 @@ const WelcomeSection = () => {
   return (
     <div id="welcome-sec">
       <h2>Who We Are?</h2>
-      <p className="content-box welcome-sec-content">
+      <p className="content-box">
         At Diamond Dental Clinic, you are our first priority. From the moment
         you walk in the door, we make it our mission to meet all your dental
         needs. We proudly serve patients in ten different languages with
         extended office hours in a convenient, central Vancouver location.
       </p>
-      <Button className="btn--alt">{"Meet Our Team"}</Button>
+      <Button className="btn">{"Meet Our Team"}</Button>
     </div>
   );
 };
