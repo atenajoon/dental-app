85114a90bf4edbffaaaabcdbde66e2cbbb670cc5
Atena Razm <atenarazm@Atenas-MacBook-Air.local>
2021-09-06 17:23:03 -0700

    updating the node module versions

--- src/components/common/Header/index.jsx
+++ src/components/common/Header/index.jsx
@@ -33,7 +33,7 @@ const Header = () => {
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
               <NavLink
-                className="nav-link active"
+                className="nav-link"
                 aria-current="page"
                 to="/home"
               >
