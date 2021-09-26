b269bce24a138d7dc82273ca58e502d2db51d513
Atena Razm <atenarazm@Atenas-MacBook-Air.local>
2021-09-26 14:33:54 -0700

    starting point of leaflet map, before debugging the error of dependancies

---
+++ src/components/common/LeafletMap/index.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+
+function LefletMap() {
+  return <div>leaflet</div>;
+}
+
+export default LefletMap;
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,8 +1,13 @@
 import React from "react";
+import LefletMap from "../components/common/LeafletMap";
+
 const Contact = () => {
   return (
     <div>
       <h1>Contact</h1>
+      <div id="mapid">
+        <LefletMap />
+      </div>
     </div>
   );
 };
