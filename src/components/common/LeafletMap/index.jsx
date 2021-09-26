7458bba5058c404b63eddb473bd6119ee7cdb7d5
Atena Razm <atenarazm@Atenas-MacBook-Air.local>
2021-09-26 15:31:55 -0700

    leaflet map displays the location of the dental clinic, full page

--- src/components/common/LeafletMap/index.jsx
+++ src/components/common/LeafletMap/index.jsx
@@ -4,14 +4,20 @@ import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 function LefletMap() {
   return (
     <div id="mapid" className="leaflet-container">
-      <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={true}>
+      <MapContainer
+        center={[49.32418387168952, -123.1143030015945]}
+        zoom={15}
+        scrollWheelZoom={true}
+      >
         <TileLayer
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <Marker position={[51.505, -0.09]}>
+        <Marker position={[49.32418387168952, -123.1143030015945]} zoom={12}>
           <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
+            <h5>Dimond Dental Clinic</h5> <br /> 1478 Marine Dr, North
+            Vancouver, BC <br />
+            V7P 1T6
           </Popup>
         </Marker>
       </MapContainer>
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -3,7 +3,7 @@ import LefletMap from "../components/common/LeafletMap";
 
 const Contact = () => {
   return (
-    <div className="container">
+    <div>
       <h1>Contact</h1>
       <div>
         <LefletMap />
